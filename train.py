57f084c54fe132500937d4dedba0cd79e84ac545
brianjkim <brianjkim0209@gmail.com>
2020-08-12 22:23:13 -0500

    simple machine learning

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from features import features
 labels = get_train_labels()
 data = features
 
+# simple machine learning using random forest classifier
 train_data, test_data, train_labels, test_labels = train_test_split(data, labels, random_state=1)
 forest = RandomForestClassifier(random_state=1)
 forest.fit(train_data, train_labels)
